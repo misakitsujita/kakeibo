20f439d5e86198a387c90c65bb2e1844ba623f29
misakitsujita <misaki.tsujita@rakus.co.jp>
2017-08-20 16:41:34 +0900

    domain

--- src/main/java/com/example/demo/domein/AdminUser.java
+++ src/main/java/com/example/demo/domein/AdminUser.java
@@ -1,8 +1,8 @@
 package com.example.demo.domein;
 
-public class User {
+public class AdminUser {
 	
-	private Integer id;
+private Integer id;
 	
 	private String name;
 	
@@ -31,7 +31,5 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
 
 }
---
+++ src/main/java/com/example/demo/domein/Payment.java
@@ -0,0 +1,59 @@
+package com.example.demo.domein;
+
+import java.util.Date;
+
+public class Payment {
+	
+	private Integer id;
+	
+	private Integer userId;
+	
+	private Integer categoryId;
+	
+	private Integer payment;
+	
+	private Date date;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public Integer getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Integer payment) {
+		this.payment = payment;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+
+}
